398ebb7b3d1c4d350dbdec15a7c5d0a72efabc98
Levelvini <viniciusfc29@gmail.com>
2025-07-08 08:48:13 -0300

    project start

---
+++ src/test/java/com/levelvini/librarymanagemet/LibraryManagemetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelvini.librarymanagemet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagemetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
